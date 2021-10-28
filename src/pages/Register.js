005b7d8da1474a94f3f17a01cfb46f3db06ced92
xongjaemin <woals99@gmail.com>
2021-10-29 02:11:10 +0900

    로그인/닉네임 등록 중복처리 빼고 끝

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -10,33 +10,29 @@ function Register(props) {
     const userInfo = location.state.userInfo; //login에서 받아온 유저 정보
     const [newNickName, setNewNickName] = useState('');
 
-    //console.log(userInfo); //유저정보 출력 테스트
+    console.log(userInfo); //유저정보 출력 테스트
 
     function nickChange(e){
         setNewNickName(e.target.value);
     }
 
     const submitClickHandler = async() => {
-        console.log(newNickName);
-
         const response = await axios.post('http://localhost:8080/auth/nickname',{
-            //email: userInfo.email,
-            nickname: newNickName
+            email: userInfo.email,
+            nickName: newNickName
         });
-        
-
-        console.log(response.data);
+        console.log(response.data.nickname);   
         userInfo.nickname = newNickName; // login 에서 받아온 userInfo에는 nickname 값이 공백이기 때문에, 닉네임 입력 후에는 다시 초기화해줘야함
 
         /* 메인으로 넘어가는 코드 필요 
             메인으로 넘어갈 때도 userInfo 들고가야함 
             userInfo 에서 nickname 값 출력하는 거까지 확인부탁합니당
         */
-            props.history.push({
-                pathname: "/main",
-                state: {userInfo: userInfo}
-            });
 
+        props.history.push({
+            pathname: "/main",
+            state: {userInfo: userInfo}
+        });
 
     }
 
