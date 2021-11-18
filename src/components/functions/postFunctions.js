8ff64a249246c218b134fe379cf52e5e1ea78ef2
NamNamju <namuju_hi@naver.com>
2021-11-19 01:35:51 +0900

    [스크랩]진행중

--- src/components/functions/postFunctions.js
+++ src/components/functions/postFunctions.js
@@ -9,13 +9,18 @@ export const postUpdate = async (updated, studyId) => {
 export const postSave = async (body) => {
   await axios.post(`http://13.209.66.117:8080/study/`, body);
 };
-export const postScrap = async (studyId) => {
-  await axios.post(`http://13.209.66.117:8080/studylist/${studyId}`, {
+export const postScrap = async (studyId, body) => {
+  await axios.post(`http://13.209.66.117:8080/studylist/${studyId}`, body, {
     params: { studyId: studyId },
   });
 };
 export const registerPost = async (studyId,body) => {
   await axios.post(`http://13.209.66.117:8080/study/${studyId}/application`, body,{
     params:{studyId:studyId}
+  });
+};
+export const postScrapDelete = async (studyId,body) => {
+  await axios.delete(`http://13.209.66.117:8080/studylist/${studyId}/cancel-like`,body,{
+    params:{studyId:studyId},
   });
 };
