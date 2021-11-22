5a0e2e8dffbe9c7679499af680335228ad4ebdd2
xongjaemin <woals99@gmail.com>
2021-11-23 04:33:39 +0900

    시간표 누르면 색 바뀜

--- src/pages/Mypage/Schedule.js
+++ src/pages/Mypage/Schedule.js
@@ -13,6 +13,24 @@ function Schedule() {
     const isManager = location.state.isManager;
     const participants = location.state.participants;
 
+    const [schedule, setSchedule] = useState(
+        [
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+        ]
+    )
+
+    function changeColor(n, i){
+        let newArr = [...schedule];
+        newArr[n][i] = !newArr[n][i];
+        setSchedule(newArr);
+    }
+
     console.log(participants);
 
     return (
@@ -42,6 +60,79 @@ function Schedule() {
                     }
                     </div>
                 </div>
+
+                <div className="scheduleContainer">
+                    <div className="dayOfWeekContainer">
+                        <div className="dayOfWeek">월</div>
+                        <div className="dayOfWeek">화</div>
+                        <div className="dayOfWeek">수</div>
+                        <div className="dayOfWeek">목</div>
+                        <div className="dayOfWeek">금</div>
+                        <div className="dayOfWeek">토</div>
+                        <div className="dayOfWeek">일</div>
+                    </div>
+                    <div className="timeContainer">
+                        {
+                            schedule[0].map((a,i)=>{
+                                return <div className="timeNumber">{i+4}</div>
+                            })
+                        }
+                        <div className="timeNumber">00</div>
+                        
+                    </div>
+                    <div className="tablesOfDayOfWeek">
+                        {
+                            schedule[0].map((a, i)=>{
+                                return <div className={schedule[0][i] == false? "timeTable" : "timeTableColored"} onClick={()=>changeColor(0, i)}/>
+                            })
+                        }
+                    </div>
+                    <div className="tablesOfDayOfWeek">
+                        {
+                            schedule[0].map((a, i)=>{
+                                return <div className={schedule[1][i] == false? "timeTable" : "timeTableColored"} onClick={()=>changeColor(1, i)}/>
+                            })
+                        }
+                    </div>
+                    <div className="tablesOfDayOfWeek">
+                        {
+                            schedule[0].map((a, i)=>{
+                                return <div className={schedule[2][i] == false? "timeTable" : "timeTableColored"} onClick={()=>changeColor(2, i)}/>
+                            })
+                        }
+                    </div>
+                    <div className="tablesOfDayOfWeek">
+                        {
+                            schedule[0].map((a, i)=>{
+                                return <div className={schedule[3][i] == false? "timeTable" : "timeTableColored"} onClick={()=>changeColor(3, i)}/>
+                            })
+                        }
+                    </div>
+                    <div className="tablesOfDayOfWeek">
+                        {
+                            schedule[0].map((a, i)=>{
+                                return <div className={schedule[4][i] == false? "timeTable" : "timeTableColored"} onClick={()=>changeColor(4, i)}/>
+                            })
+                        }
+                    </div>
+                    <div className="tablesOfDayOfWeek">
+                        {
+                            schedule[0].map((a, i)=>{
+                                return <div className={schedule[5][i] == false? "timeTable" : "timeTableColored"} onClick={()=>changeColor(5, i)}/>
+                            })
+                        }
+                    </div>
+                    <div className="tablesOfDayOfWeek">
+                        {
+                            schedule[0].map((a, i)=>{
+                                return <div className={schedule[6][i] == false? "timeTable" : "timeTableColored"} onClick={()=>changeColor(6, i)}/>
+                            })
+                        }
+                    </div>
+                </div>
+
+                <div className="scheduleBtns">수정</div>
+                <div className="scheduleBtns">저장</div>
 
             </div>
 
